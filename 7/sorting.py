c5da424b2a203e373845899cb1b92cef6919576c
Hyunwoo Lee <hwlee@kentech.ac.kr>
2025-11-17 13:07:13 +0900

    upload the problem 7

---
+++ 7/sorting.py
@@ -0,0 +1,16 @@
+import logging
+
+def bubble(lst):
+    logging.debug("Bubble sort function")
+    pass
+
+def insertion(lst):
+    logging.debug("Insertion sort function")
+    pass
+
+def merge(l1, l2):
+    logging.debug("Merge sort function")
+    pass
+
+def mergesort(lst):
+    pass
